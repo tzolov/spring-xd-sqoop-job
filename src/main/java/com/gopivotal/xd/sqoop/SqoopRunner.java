ddc4faf2008e7fefe218fe15a3b4c780b2290236
tzolov <christian.tzolov@gmail.com>
2013-11-07 10:32:42 +0100

    clean the SqoopRunner code

--- src/main/java/com/gopivotal/xd/sqoop/SqoopRunner.java
+++ src/main/java/com/gopivotal/xd/sqoop/SqoopRunner.java
@@ -4,20 +4,19 @@ import org.apache.sqoop.Sqoop;
 
 public class SqoopRunner {
 
-	// sqoop export --connect jdbc:mysql://192.168.178.13/sqoop_test --username gpadmin --password gpadmin --table cdr_orig --export-dir /xd/ti/data/out/prepared/dvcallreceived.csv
+	// sqoop export --connect jdbc:mysql://192.168.178.13/sqoop_test --username
+	// gpadmin --password gpadmin --table cdr_orig --export-dir
+	// /xd/ti/data/out/prepared/dvcallreceived.csv
 
 	public static void main(String[] args) {
-		String[] args2 = new String[] { "export", "--connect",
-				"jdbc:mysql://192.168.178.13/sqoop_test", "--username", "gpadmin", "--password", "gpadmin",
-				"--table", "cdr_orig", "--export-dir",
-				"/xd/ti/data/out/prepared/dvcallreceived.csv", "" };
-		String[] args3 = args[0].split(" ");
-		final int ret = Sqoop.runTool(args3);
+
+		String[] sqoopArguments = args[0].split(" ");
+
+		final int ret = Sqoop.runTool(sqoopArguments);
+
 		if (ret != 0) {
 			throw new RuntimeException("Sqoop failed - return code "
 					+ Integer.toString(ret));
 		}
 	}
-
-
 }
